ce0ee7134d471bb77388c682b2dc31ac677e62fa
Tony Siu <thefool363@gmail.com>
2020-02-09 22:03:48 +0800

    making a stock market app

---
+++ predictor/apps.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.apps import AppConfig
+from django.conf import settings
+import os
+import pickle
+
+
+class EventsConfig(AppConfig):
+    name = 'events'
+
+class PredictorConfig(AppConfig):
+
+    # create path to models
+    path = os.path.join(settings.MODELS,'models.pk1')
+
+    # load models into separate variables
+    # these will be accessible via this class 
+    with open(path,'rb') as pickled:
+        data = pickle.load(pickled)
+
+    #  regressor = data['regressor']
+    # vectorizer = data['vectorizer']
+
+    
---
+++ predictor/views.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+from .apps import PredictorConfig
+from django.http import HttpResponse,JsonResponse,request
+from rest_framework.views import APIView
+
+import yfinance as yf 
+import pandas as pd 
+import xgboost as xgb
+import requests
+import json 
+
+Alpaca_API_key = 'PKLQHCF2P3VRXDC4LBBW'
+
+Alpaca_secret_key = 'vBJbM3zLE42EQQ3xX0MXTajddGOyUU6Hd4b7zw0m'
+
+Endpoint_url = 'https://paper-api.alpaca.markets'
+
+Account_url = f'{Endpoint_url}/v2/account'
+
+Order_url = f'{Endpoint_url}/v2/orders'
+
+Headers = {'APCA-API-KEY-ID':Alpaca_API_key,'APCA-API-SECRET-KEY':Alpaca_secret_key}
+
+r = requests.get(Account_url,headers=Headers)
+
+dic_r = json.loads(r.content)
+
+def yahoo_finance_history(request):
+    if request.method == 'GET':
+        requested_stock = yf.Ticker(request.form.get('stock'))
+        history = requested_stock.history(period='max')
+        stock = pd.DataFrame(history)
+        stock_changes = stock.pct_change()
+        stock_changes.drop(stock_changes.index[0],inplace=True)
+        stock_matrix = pd.concat([stock_changes.Close.shift(-i) for i in range(100)],axis=1)
+        stock_matrix.drop(stock_matrix.index[-99:],inplace=True)
+        stock_matrix.columns = [ f'Day {i+1}'for i in range(len(stock_matrix.columns))]
+        return HttpResponse(stock_matrix)
+
+# Create your views here.
+def Alpaca_order_manager(request,symbol,qty,side,tpe,time_in_force):
+    data = {
+        'symbol':symbol,
+        'qty':qty,
+        'side':side,
+        'type':tpe,
+        'time_in_force':time_in_force
+    }
+    r = requests.post(Order_url,json=data,headers=Headers)
+    return json.loads(r.content)
+
+class call_model(APIView):
+    def get(self,request):
+        if request.method == 'GET':
+
+            #get stock name from request
+            stock = request.GET.get('stock')
+
+            # predict stock d test data
+            prediction = PredictorConfig.predict(stock)
+
+            return HttpResponse(prediction)
