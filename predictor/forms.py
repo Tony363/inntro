34f8c625b7d5f2834b62123f15087374aeceec81
Tony Siu <thefool363@gmail.com>
2020-02-10 20:21:39 +0800

    trying to make a UI

--- predictor/forms.py
+++ predictor/forms.py
@@ -1,9 +1,10 @@
-from django.db import models
+from django import forms
+from .models import Index
 
 
-class Index(models.Model):
+class Index_form(models.Model):
     stock = models.CharField(max_length=100)
-    checkbox = models.BooleanField(default=False)
+    # checkbox = models.BooleanField(default=False)
     
 
     def __str__(self):
