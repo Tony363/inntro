aa3d73cebd8e0e1b7209193c16cbd66acd80632f
Tony Siu <thefool363@gmail.com>
2020-02-10 22:59:09 +0800

    trying to predict now

--- mycalendar/settings.py
+++ mycalendar/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'predictor'
+    'predictor',
+    'crispy_forms'
 ]
 
 MIDDLEWARE = [
