5e0d2bc5e97b5ac17542f2c9201855cad7756ebf
Tony Siu <thefool363@gmail.com>
2020-02-18 19:42:46 +0800

    thats for today

--- predictor/views.py
+++ predictor/views.py
@@ -111,13 +111,17 @@ def visualization(request):
         df_shap_XGB_train = pd.DataFrame(shap_values_XGB_train)
 
         shap.force_plot(explainer.expected_value,shap_values_XGB_test[0],X_test.iloc[[0]],show=False,matplotlib=True).savefig('/home/tony/Desktop/github_repos/inntro/predictor/static/image/initial.png')
-        # shap.force_plot(explainer.expected_value,shap_values_XGB_test,X_test).savefig('/home/tony/Desktop/github_repos/inntro/predictor/static/image/wave_plot.png')
+        
         s_plot = plt.figure()
-        shap.summary_plot(shap_values_XGB_train,X_train,plot_type='bar')
+        shap.force_plot(explainer.expected_value,shap_values_XGB_test,X_test,show=False)
+        s_plot.savefig('/home/tony/Desktop/github_repos/inntro/predictor/static/image/wave_plot.png')
+
+        s_plot = plt.figure()
+        shap.summary_plot(shap_values_XGB_train,X_train,plot_type='bar',show=False)
         s_plot.savefig('/home/tony/Desktop/github_repos/inntro/predictor/static/image/barplot.png')
        
         s_plot = plt.figure()
-        shap.summary_plot(shap_values_XGB_train,X_train)
+        shap.summary_plot(shap_values_XGB_train,X_train,show=False)
         s_plot.savefig('/home/tony/Desktop/github_repos/inntro/predictor/static/image/shap_value.png')
      
         # shap.dependence_plot('Days_1',shap_values_XGB_train,X_train).savefig('/home/tony/Desktop/github_repos/inntro/predictor/static/image/scatter.png')
