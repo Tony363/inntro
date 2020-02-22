3aade4ca8d5b7f4882b1263bac84bb40d4a0d8d5
tony363 <pysolver33@gmail.com>
2020-02-22 16:31:36 +0800

    little more features

--- predictor/urls.py
+++ predictor/urls.py
@@ -2,5 +2,11 @@ from django.urls import include, path
 from . import views
 
 urlpatterns = [
-    path('visualization/',views.visualization,name='visualization')
+    path('visualization/',views.visualization,name='visualization'),
+    path('visualization/X_train/',views.X_train,name='X_train'),
+    path('visualization/X_test/',views.X_test,name='X_test'),
+    path('visualization/y_train/',views.y_train,name='y_train'),
+    path('visualization/y_test/',views.y_test,name='y_test'),
+    path('visualization/stock/',views.stock,name='stock'),
+    path('visualization/prediction/',views.prediction,name='prediction'),
 ]
